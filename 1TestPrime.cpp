3db11fb98d879c8af446bae8a230999bbcb813bb
Welemhret Welay Baraki <weldie.ed@gmail.com>
2021-02-21 04:51:48 +0100

    Update 1TestPrime.cpp

--- 1TestPrime.cpp
+++ 1TestPrime.cpp
@@ -1,6 +1,28 @@
 /* 
-This is multiline comment. 
-Thanks for Being part of 
- 
+Prime Number Testing 
+The First Program from the Udemy competitive programming language.  
 */
+#include<iostream> 
+using namespace std;
  
+int main()
+{
+int n;
+cout<<"Enter a positive number for primary testing! "<<endl;
+cin >> n;
+for( int i; i<n ;i++)
+{
+	if(n%i==0){
+	cout<<"The Number is not prime"<<endl;
+	}
+	else 
+	  {
+	  cout<<"The number is Prime number"<<endl;
+	  }
+}  
+return 0;
+}
+
+
+
+
